15fe538273d1b86e8f86693422d6e9755fe0f9e1
MD Nasir Uddin Rubel <96114807+nurubel1819@users.noreply.github.com>
2022-08-11 13:13:47 +0600

    Update all_method_for_contest.cpp

--- all_method_for_contest.cpp
+++ all_method_for_contest.cpp
@@ -1,40 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long int
 /*
-int arr[100001];
-int tree[100001 * 4];
+ll arr[99999999];
+ll tree[100001 * 4];
 //segment tree simply build(node=1,b=array's first index , e =array's last index)
-void init(int node, int b, int e)
+void init(ll node, ll b, ll e)
 {
     if (b == e) {
         tree[node] = arr[b];
         return;
     }
-    int Left = node * 2;
-    int Right = node * 2 + 1;
-    int mid = (b + e) / 2;
+    ll Left = node * 2;
+    ll Right = node * 2 + 1;
+    ll mid = (b + e) / 2;
     init(Left, b, mid);
     init(Right, mid + 1, e);
     tree[node] = tree[Left] + tree[Right];//sumation of range
     //tree[node] = max(tree[Left],tree[Right]);//big element of range
 }
 //segment tree for output (node = 1 , b = array's first index , e = array's last index , i = range'f first index, j = range last index)
-int query(int node, int b, int e, int i, int j)
+ll query(ll node, ll b, ll e, ll i, ll j)
 {
     if (i > e || j < b)
         return 0; //বাইরে চলে গিয়েছে
     if (b >= i && e <= j)
         return tree[node]; //রিলেভেন্ট সেগমেন্ট
-    int Left = node * 2; //আরো ভাঙতে হবে
-    int Right = node * 2 + 1;
-    int mid = (b + e) / 2;
-    int p1 = query(Left, b, mid, i, j);
-    int p2 = query(Right, mid + 1, e, i, j);
+    ll Left = node * 2; //আরো ভাঙতে হবে
+    ll Right = node * 2 + 1;
+    ll mid = (b + e) / 2;
+    ll p1 = query(Left, b, mid, i, j);
+    ll p2 = query(Right, mid + 1, e, i, j);
     return p1 + p2; //বাম এবং ডান পাশের যোগফল , sum of range
     //return max(p1,p2);// big number in range
 }
 //segment tree for update (node = 1 , b = array's first index , e = array's last index , i = range'f first index, j = range last index)
-void update(int node, int b, int e, int i, int newvalue)
+void update(ll node, ll b, ll e, ll i, ll newvalue)
 {
     if (i > e || i < b)
         return; //বাইরে চলে গিয়েছে
@@ -42,15 +43,17 @@ void update(int node, int b, int e, int i, int newvalue)
         tree[node] = newvalue;
         return;
     }
-    int Left = node * 2; //আরো ভাঙতে হবে
-    int Right = node * 2 + 1;
-    int mid = (b + e) / 2;
+    ll Left = node * 2; //আরো ভাঙতে হবে
+    ll Right = node * 2 + 1;
+    ll mid = (b + e) / 2;
     update(Left, b, mid, i, newvalue);
     update(Right, mid + 1, e, i, newvalue);
     tree[node] = tree[Left] + tree[Right];// sum of element of range
     //tree[node] = max(tree[Left],tree[Right]);//big element of range
 }
 */
+//------------------------------------------------------------------------
+
 long long gcd(long long int a, long long int b)//gosagu
 {
     if (b == 0)
